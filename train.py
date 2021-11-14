53f0c6c9fbf28e34c209fded3480944b4a127fe3
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-11-14 23:16:16 +0800

    Update train.py

--- train.py
+++ train.py
@@ -195,7 +195,7 @@ if __name__ == "__main__":
             validation_steps    = epoch_step_val,
             epochs              = Epoch,
             initial_epoch       = Freeze_Epoch,
-            use_multiprocessing = True if num_workers != 0 else False,
+            use_multiprocessing = True if num_workers > 1 else False,
             workers             = num_workers,
             callbacks           = [logging, checkpoint, reduce_lr, early_stopping, loss_history]
         )
