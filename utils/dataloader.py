94d28f6ee833c30d39831c2b9912142e9ad775c7
Bubbliiiing <3323290568@qq.com>
2022-03-13 14:06:47 +0800

    update a lot

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -41,13 +41,13 @@ class ClsDatasets(keras.utils.Sequence):
         Y_train = np_utils.to_categorical(np.array(Y_train), num_classes = self.num_classes)   
         return X_train, Y_train
 
-    def on_epoch_begin(self):
+    def on_epoch_end(self):
         shuffle(self.annotation_lines)
 
     def rand(self, a=0, b=1):
         return np.random.rand()*(b-a) + a
 
-    def get_random_data(self, image, input_shape, jitter=.3, hue=.1, sat=1.5, val=1.5, random=True):
+    def get_random_data(self, image, input_shape, jitter=.3, hue=.1, sat=0.7, val=0.3, random=True):
         #------------------------------#
         #   读取图像并转换成RGB图像
         #------------------------------#
@@ -78,8 +78,8 @@ class ClsDatasets(keras.utils.Sequence):
         #------------------------------------------#
         #   对图像进行缩放并且进行长和宽的扭曲
         #------------------------------------------#
-        new_ar = w/h * self.rand(1-jitter,1+jitter)/self.rand(1-jitter,1+jitter)
-        scale = self.rand(.75, 1.25)
+        new_ar = iw/ih * self.rand(1-jitter,1+jitter) / self.rand(1-jitter,1+jitter)
+        scale = self.rand(0.75, 1.5)
         if new_ar < 1:
             nh = int(scale*h)
             nw = int(nh*new_ar)
@@ -93,7 +93,7 @@ class ClsDatasets(keras.utils.Sequence):
         #------------------------------------------#
         dx = int(self.rand(0, w-nw))
         dy = int(self.rand(0, h-nh))
-        new_image = Image.new('RGB', (w,h), (128,128,128))
+        new_image = Image.new('RGB', (w,h), (128, 128, 128))
         new_image.paste(image, (dx, dy))
         image = new_image
 
@@ -108,19 +108,27 @@ class ClsDatasets(keras.utils.Sequence):
             angle = np.random.randint(-15,15)
             a,b = w/2,h/2
             M = cv2.getRotationMatrix2D((a,b),angle,1)
-            image = cv2.warpAffine(np.array(image), M, (w,h), borderValue=[128,128,128]) 
+            image = cv2.warpAffine(np.array(image), M, (w,h), borderValue=[128, 128, 128]) 
 
-        #------------------------------------------#
-        #   色域扭曲
-        #------------------------------------------#
-        hue = self.rand(-hue, hue)
-        sat = self.rand(1, sat) if self.rand()<.5 else 1/self.rand(1, sat)
-        val = self.rand(1, val) if self.rand()<.5 else 1/self.rand(1, val)
-        x = cv2.cvtColor(np.array(image,np.float32)/255, cv2.COLOR_RGB2HSV)
-        x[..., 1] *= sat
-        x[..., 2] *= val
-        x[x[:,:, 0]>360, 0] = 360
-        x[:, :, 1:][x[:, :, 1:]>1] = 1
-        x[x<0] = 0
-        image_data = cv2.cvtColor(x, cv2.COLOR_HSV2RGB)*255
+        image_data      = np.array(image, np.uint8)
+        #---------------------------------#
+        #   对图像进行色域变换
+        #   计算色域变换的参数
+        #---------------------------------#
+        r               = np.random.uniform(-1, 1, 3) * [hue, sat, val] + 1
+        #---------------------------------#
+        #   将图像转到HSV上
+        #---------------------------------#
+        hue, sat, val   = cv2.split(cv2.cvtColor(image_data, cv2.COLOR_RGB2HSV))
+        dtype           = image_data.dtype
+        #---------------------------------#
+        #   应用变换
+        #---------------------------------#
+        x       = np.arange(0, 256, dtype=r.dtype)
+        lut_hue = ((x * r[0]) % 180).astype(dtype)
+        lut_sat = np.clip(x * r[1], 0, 255).astype(dtype)
+        lut_val = np.clip(x * r[2], 0, 255).astype(dtype)
+
+        image_data = cv2.merge((cv2.LUT(hue, lut_hue), cv2.LUT(sat, lut_sat), cv2.LUT(val, lut_val)))
+        image_data = cv2.cvtColor(image_data, cv2.COLOR_HSV2RGB)
         return image_data
